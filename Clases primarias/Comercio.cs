39d2e9c46c639396d35a30d19898f72307e813ae
Daniel Adrián Grinman <dan.gr04@live.com>
2016-09-27 08:57:21 -0300

    asdf

---
+++ Clases primarias/Comercio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_primarias
+{
+    class Comercio: Inmueble
+    {
+        public Comercio() { }
+
+        public override int id { get; set; }
+
+        public override bool aguaPotable { get; set; }
+
+        public override bool electricidad { get; set; }
+
+        public override bool gas { get; set; }
+
+        public override string observaciones { get; set; }
+
+        public override bool disponible { get; set; }
+
+        public override string estadoI { get; set; }
+    }
+}
